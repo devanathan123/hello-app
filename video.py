6ba1b3911beb1992230d1218ebdc473b5f444313
devanathan123 <60214080+devanathan123@users.noreply.github.com>
2024-04-16 11:21:35 +0530

    Update video.py

--- video.py
+++ video.py
@@ -27,9 +27,10 @@ import numpy as np
 st.title("Streamlit Webcam Player")
 
 video_url = "https://firebasestorage.googleapis.com/v0/b/fir-ec695.appspot.com/o/t-13%2Finput_side.mp4?alt=media&token=0cf29a63-b98b-451a-a9f5-7e2d71caf9c3"  # Replace "your-file-id" with the actual file ID
-cap = cv2.VideoCapture(video_url)
     
 def main():
+    cap = cv2.VideoCapture(video_url)
+
     load = st.button("STOP")
     stframe = st.empty()
 
