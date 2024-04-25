509e6bd7689b0bc070d8064f80ef97fe08b09ab2
devanathan123 <60214080+devanathan123@users.noreply.github.com>
2024-04-25 22:37:22 +0530

    Update video.py

--- video.py
+++ video.py
@@ -14,39 +14,39 @@
 # if __name__ == "__main__":
 #     main()
 #VIDEO CAPTURE------------------------------------------------
-# import streamlit as st
-# import cv2
-# import numpy as np
+import streamlit as st
+import cv2
+import numpy as np
 
-# st.title("Streamlit Webcam Player")
+st.title("Streamlit Webcam Player")
     
-# def main():
-#     video_url = "https://firebasestorage.googleapis.com/v0/b/fir-ec695.appspot.com/o/t-13%2Finput_side.mp4?alt=media&token=0cf29a63-b98b-451a-a9f5-7e2d71caf9c3"  # Replace "your-file-id" with the actual file ID
-#     cap = cv2.VideoCapture(video_url)
-#     load = st.button("STOP")
-#     stframe = st.empty()
+def main():
+    video_url = "https://firebasestorage.googleapis.com/v0/b/fir-ec695.appspot.com/o/t-13%2Finput_side.mp4?alt=media&token=0cf29a63-b98b-451a-a9f5-7e2d71caf9c3"  # Replace "your-file-id" with the actual file ID
+    cap = cv2.VideoCapture(video_url)
+    load = st.button("STOP")
+    stframe = st.empty()
 
-#     left_limits1 = [0, 450, 1920,450]
-#     left_limits2 = [0, 500, 1920,500]
+    left_limits1 = [0, 450, 1920,450]
+    left_limits2 = [0, 500, 1920,500]
 
-#     while not load:
-#         success, img = cap.read()
+    while not load:
+        success, img = cap.read()
 
-#         cv2.line(img, (left_limits1[0], left_limits1[1]), (left_limits1[2], left_limits1[3]), (0, 0, 255), 5)
-#         cv2.line(img, (left_limits2[0], left_limits2[1]), (left_limits2[2], left_limits2[3]), (255, 0, 0), 5)
+        cv2.line(img, (left_limits1[0], left_limits1[1]), (left_limits1[2], left_limits1[3]), (0, 0, 255), 5)
+        cv2.line(img, (left_limits2[0], left_limits2[1]), (left_limits2[2], left_limits2[3]), (255, 0, 0), 5)
 
 
-#         if not success:
-#             break
+        if not success:
+            break
  
-#         # Display the frame in the video element
-#         stframe.image(img, channels='BGR', use_column_width=True)
+        # Display the frame in the video element
+        stframe.image(img, channels='BGR', use_column_width=True)
 
     
-#     st.title("DONE!!")
+    st.title("DONE!!")
 
-# if __name__ == "__main__":
-#     main()
+if __name__ == "__main__":
+    main()
 #CAM PERMISSION---------------------------------------
 # import streamlit as st
 # from streamlit_webrtc import webrtc_streamer
@@ -108,24 +108,24 @@
 # if __name__ == "__main__":
 #     main()
 # STORAGE --> webrtc ---------------------------------------
-import streamlit as st
-import os
+# import streamlit as st
+# import os
 
-def main():
-    st.title("Streamlit Video Player")
+# def main():
+#     st.title("Streamlit Video Player")
 
-    uploaded_file = st.file_uploader("Upload a video", type=["mp4", "avi"])
+#     uploaded_file = st.file_uploader("Upload a video", type=["mp4", "avi"])
 
-    if uploaded_file is not None:
-        # Save the uploaded file to a temporary location
-        with open("temp_video.mp4", "wb") as f:
-            f.write(uploaded_file.read())
+#     if uploaded_file is not None:
+#         # Save the uploaded file to a temporary location
+#         with open("temp_video.mp4", "wb") as f:
+#             f.write(uploaded_file.read())
 
-        # Display the video using st.video
-        st.video("temp_video.mp4")
+#         # Display the video using st.video
+#         st.video("temp_video.mp4")
 
-        # Remove the temporary file after displaying the video
-        os.remove("temp_video.mp4")
+#         # Remove the temporary file after displaying the video
+#         os.remove("temp_video.mp4")
 
-if __name__ == "__main__":
-    main()
+# if __name__ == "__main__":
+#     main()
