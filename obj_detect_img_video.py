0e98b35b27d34980d992462f6b5ec078ba0706cf
devanathan123 <60214080+devanathan123@users.noreply.github.com>
2024-04-29 02:42:17 +0530

    Update obj_detect_img_video.py

--- obj_detect_img_video.py
+++ obj_detect_img_video.py
@@ -195,7 +195,7 @@ def load_product_counter(video_name_s,video_name_t, kpi1_text, kpi2_text, kpi3_t
                     boxes = res_t[0].boxes.xywh
                     clss = res_t[0].boxes.cls.tolist()
                     st.title(res_t[0].boxes.id)
-                    track_id = res_t[0].boxes.id.int().item()
+                    track_id = int(np.array(res_t[0].boxes.id)[0])
 
                     # track_ids = res_t[0].boxes.id.tolist()
             
