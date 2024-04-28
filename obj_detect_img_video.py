aecb20e049de31a6bcd9686382233c70c5f4e748
devanathan123 <60214080+devanathan123@users.noreply.github.com>
2024-04-28 19:29:33 +0530

    Update obj_detect_img_video.py

--- obj_detect_img_video.py
+++ obj_detect_img_video.py
@@ -102,8 +102,8 @@ def load_product_counter(video_name_s,video_name_t, kpi1_text, kpi2_text, kpi3_t
 
 
     #!!!!!!! -details !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-    tb=shop3
-    product=Mango
+    tb="shop3"
+    product="Mango"
     doc_ref=db.collection(tb).document(product)
     doc=doc_ref.get()
     if doc.exists:
