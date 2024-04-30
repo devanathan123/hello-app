e5829e1612d41d7892cfa3cbf4b24cd18260da84
devanathan123 <60214080+devanathan123@users.noreply.github.com>
2024-04-30 23:18:56 +0530

    Update obj_detect_img_video.py

--- obj_detect_img_video.py
+++ obj_detect_img_video.py
@@ -120,7 +120,7 @@ def load_product_counter(video_name_s,video_name_t, kpi1_text, kpi2_text, kpi3_t
 
       # To Update data:----------------------
       #doc_ref.update({'Stock': firestore.Increment(1)})
-      doc_ref.update({'Stock': firebase.firestore.FieldValue.increment(1)})
+      doc_ref.update({'Stock': firestore.increment(1)})
       doc=doc_ref.get()
       doc_data = doc.to_dict()
       field_value = doc_data.get('Stock')
